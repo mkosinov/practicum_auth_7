fe4bc8124c71f2fb9f99fcd45ad90cd19700eb05
Boris <bbaranov@ptsecurity.com>
2024-05-06 23:14:20 +0300

    Create partitions

--- auth/src/models/user_history.py
+++ auth/src/models/user_history.py
@@ -1,7 +1,7 @@
 import uuid
 from datetime import datetime
 
-from sqlalchemy import Column, DateTime, ForeignKey, String
+from sqlalchemy import Column, DateTime, ForeignKey, String, text, UniqueConstraint
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 
@@ -10,6 +10,12 @@ from db.postgres.session_handler import session_handler
 
 class UserHistoryModel(session_handler.base):
     __tablename__ = "user_history"
+    __table_args__ = (
+        UniqueConstraint('id', 'action'),
+        {
+            'postgresql_partition_by': 'LIST (action)',
+        }
+    )
 
     id = Column(
         UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True
@@ -20,7 +26,7 @@ class UserHistoryModel(session_handler.base):
     device_id = Column(
         UUID, ForeignKey("device.id", ondelete="SET NULL"), nullable=False
     )
-    action = Column(String(50), default="login", nullable=False)
+    action = Column(String(50), default="login", primary_key=True, nullable=False)
     ip = Column(String(39), default="", nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
 
